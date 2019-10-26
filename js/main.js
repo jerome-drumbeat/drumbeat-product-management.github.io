09c47518fbc18388066ddf026c242ca543986289
Jérôme Van De Sande <jerome@drumbeat.be>
2019-10-27 00:27:16 +0200

    Added client logos
    - Added client logos
    - Removed unused images
    - Removed unused JS

--- js/main.js
+++ js/main.js
@@ -10,17 +10,8 @@ jQuery(document).on('ready', function () {
 
     //Fix for Split Section Backgroun on small resolutions
     setBackgroundColorSplitSection();
-    //Fix for V-Skills
-    verticalSkills();
     //Fix z-index
     zIndexSectionFix();
-    //Member Content Load
-    memberContentLoadOnClick();
-    //Portfolio Item Load
-    portfolioItemContentLoadOnClick();
-    //PrettyPhoto initial
-    setPrettyPhoto();
-
 
     //Fix for Menu
     jQuery(".header-holder").sticky({topSpacing: 0});
@@ -57,13 +48,6 @@ jQuery(document).on('ready', function () {
 
     jQuery(window).scrollTop(1);
     jQuery(window).scrollTop(0);
-
-    jQuery('.single-post .num-comments a').on('click', function (e) {
-        e.preventDefault();
-        jQuery('html, body').animate({scrollTop: jQuery(this.hash).offset().top}, 2000);
-        return false;
-    });
-
 
     //Placeholder show/hide
     jQuery('input, textarea').on("focus", function () {
@@ -160,17 +144,6 @@ var setBackgroundColorSplitSection = function () {
     });
 };
 
-var verticalSkills = function () {
-    jQuery(".v-skill").each(function (i) {
-        skillsColor = jQuery(this).find('span').data("color");
-        skillsFill = jQuery(this).find('span').html();
-        jQuery(this).find('.v-skill-fill').css("background-color", skillsColor).width(skillsFill).height(skillsFill);
-        jQuery(this).find('.v-skill-text').css("bottom", skillsFill);
-        jQuery(this).find('span').css("color", skillsColor);
-    });
-};
-
-
 var multiClickFunctionStop = function (e) {
     jQuery('#toggle').off("click");
     jQuery('#toggle').toggleClass("on");
@@ -241,149 +214,6 @@ function zIndexSectionFix() {
     jQuery('.page-template-onepage').find('.section-wrapper').each(function () {
         jQuery(this).css('zIndex', numSection);
         numSection--;
-    });
-}
-
-function memberContentLoadOnClick() {
-    jQuery('.ajax-member-content').on('click', function (e) {
-        e.preventDefault();
-        var memberID = jQuery(this).data('id');
-        jQuery(this).find('.member-mask').addClass('animate-plus');
-        jQuery('html, body').animate({scrollTop: jQuery('#team-holder').offset().top - 120}, 300);
-        if (jQuery("#mcw-" + memberID).length) //Check if is allready loaded
-        {
-            setTimeout(function () {
-                jQuery('.member-holder').addClass('hide');
-                setTimeout(function () {
-                    jQuery("#mcw-" + memberID).addClass('show');
-                    jQuery('.team-load-content-holder').addClass('show');
-                    jQuery('.member-mask').removeClass('animate-plus');
-                    jQuery('.member-holder').hide();
-                }, 300);
-            }, 300);
-        } else {
-            loadMemberContent(memberID);
-        }
-    });
-}
-
-function loadMemberContent(memberID) {
-    jQuery.ajax({
-        url: jQuery('.ajax-member-content[data-id="' + memberID + '"]').attr('href'),
-        type: 'POST',
-        success: function (html) {
-            var getHtml = jQuery(html).find(".member-item-wrapper").html();
-            jQuery('.team-load-content-holder').append('<div id="mcw-' + memberID + '" class="member-content-wrapper">' + getHtml + '</div>');
-            if (!jQuery("#mcw-" + memberID + " .close-icon").length) {
-                jQuery("#mcw-" + memberID).prepend('<div class="close-icon"></div>');
-            }
-            jQuery("#mcw-" + memberID).imagesLoaded(function () {
-                verticalSkills();
-                imageSliderSettings();
-                jQuery(".site-content").fitVids(); //Fit Video
-                jQuery('.member-holder').addClass('hide');
-                setTimeout(function () {
-                    jQuery("#mcw-" + memberID).addClass('show');
-                    jQuery('.team-load-content-holder').addClass('show');
-                    jQuery('.member-mask').removeClass('animate-plus');
-                    jQuery('.member-holder').hide();
-                }, 300);
-                jQuery('#team-holder .close-icon').on('click', function (e) {
-                    jQuery('.team-load-content-holder').addClass("viceversa");
-                    jQuery('.member-holder').css('display', 'block');
-                    setTimeout(function () {
-                        jQuery('#mcw-' + memberID).removeClass('show');
-                        jQuery('.team-load-content-holder').removeClass('viceversa show');
-                        jQuery('.member-holder').removeClass('hide');
-                    }, 300);
-                    // setTimeout(function () {
-                    //    jQuery('html, body').animate({scrollTop: jQuery('#team-holder').offset().top - 150}, 400);
-                    // }, 500);
-                });
-            });
-        }
-    });
-    return false;
-}
-
-var portfolioItemContentLoadOnClick = function () {
-    jQuery('.ajax-portfolio').on('click', function (e) {
-        e.preventDefault();
-        var portfolioItemID = jQuery(this).data('id');
-        jQuery(this).addClass('animate-plus');
-        if (jQuery("#pcw-" + portfolioItemID).length) //Check if is allready loaded
-        {
-            jQuery('html, body').animate({scrollTop: jQuery('#portfolio-wrapper').offset().top - 150}, 400);
-            setTimeout(function () {
-                jQuery('#portfolio-grid, .more-posts-portfolio-holder').addClass('hide');
-                setTimeout(function () {
-                    jQuery("#pcw-" + portfolioItemID).addClass('show');
-                    jQuery('.portfolio-load-content-holder').addClass('show');
-                    jQuery('.ajax-portfolio').removeClass('animate-plus');
-                    jQuery('#portfolio-grid, .more-posts-portfolio-holder').hide();
-                }, 300);
-            }, 500);
-        } else {
-            loadPortfolioItemContent(portfolioItemID);
-        }
-    });
-}
-
-function loadPortfolioItemContent(portfolioItemID) {
-    jQuery.ajax({
-        url: jQuery('.ajax-portfolio[data-id="' + portfolioItemID + '"]').attr('href'),
-        type: 'POST',
-        success: function (html) {
-            var getPortfolioItemHtml = jQuery(html).find(".portfolio-item-wrapper").html();
-            jQuery('.portfolio-load-content-holder').append('<div id="pcw-' + portfolioItemID + '" class="portfolio-content-wrapper">' + getPortfolioItemHtml + '</div>');
-            if (!jQuery("#pcw-" + portfolioItemID + " .close-icon").length) {
-                jQuery("#pcw-" + portfolioItemID).prepend('<div class="close-icon"></div>');
-            }
-            jQuery('html, body').animate({scrollTop: jQuery('#portfolio-wrapper').offset().top - 150}, 400);
-            setTimeout(function () {
-                jQuery("#pcw-" + portfolioItemID).imagesLoaded(function () {
-                    verticalSkills();
-                    imageSliderSettings();
-                    jQuery(".site-content").fitVids(); //Fit Video
-                    jQuery('#portfolio-grid, .more-posts-portfolio-holder').addClass('hide');
-                    setTimeout(function () {
-                        jQuery("#pcw-" + portfolioItemID).addClass('show');
-                        jQuery('.portfolio-load-content-holder').addClass('show');
-                        jQuery('.ajax-portfolio').removeClass('animate-plus');
-                        jQuery('#portfolio-grid').hide();
-                    }, 300);
-                    jQuery('.close-icon').on('click', function (e) {
-                        var portfolioReturnItemID = jQuery(this).closest('.portfolio-content-wrapper').attr("id").split("-")[1];
-                        jQuery('.portfolio-load-content-holder').addClass("viceversa");
-                        jQuery('#portfolio-grid, .more-posts-portfolio-holder').css('display', 'block');
-                        setTimeout(function () {
-                            jQuery('#pcw-' + portfolioReturnItemID).removeClass('show');
-                            jQuery('.portfolio-load-content-holder').removeClass('viceversa show');
-                            jQuery('#portfolio-grid, .more-posts-portfolio-holder').removeClass('hide');
-                        }, 300);
-                        setTimeout(function () {
-                            jQuery('html, body').animate({scrollTop: jQuery('#p-item-' + portfolioReturnItemID).offset().top - 150}, 400);
-                        }, 500);
-                    });
-                });
-            }, 500);
-        }
-    });
-    return false;
-}
-
-function setPrettyPhoto() {
-    jQuery('a[data-rel]').each(function () {
-        jQuery(this).attr('rel', jQuery(this).data('rel'));
-    });
-    jQuery("a[rel^='prettyPhoto']").prettyPhoto({
-        slideshow: false, /* false OR interval time in ms */
-        overlay_gallery: false, /* If set to true, a gallery will overlay the fullscreen image on mouse over */
-        default_width: 1280,
-        default_height: 720,
-        deeplinking: false,
-        social_tools: false,
-        iframe_markup: '<iframe src ="{path}" width="{width}" height="{height}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
     });
 }
 
